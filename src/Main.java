811e54480da10247dc5daeb68a0a1aaf984308a5
BT_4N2_12 <farianf2@nycstudents.net>
2023-09-19 13:50:55 -0400

    Completed Party amt and total price amt

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+
+import java.util.Scanner;
+public class Main {
+    public static void main(String[] args) {
+        Scanner totalPpl = new Scanner(System.in);
+        System.out.println("How man people in your party: ");
+        String totalPrice = totalPpl.nextLine();
+        int totalPplTwo = Integer.parseInt(totalPrice);
+
+
+        Scanner billAmt = new Scanner(System.in);
+        System.out.println("What is the total amount: ");
+        String totalAmt = billAmt.nextLine();
+        double totalAmtTwo = Double.parseDouble(totalAmt);
+
+
+        Scanner tipPercent = new Scanner(System.in);
+        
+
+
+
+    }
+}
